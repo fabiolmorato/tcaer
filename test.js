2f8b2899c504d22d7a41e618b7878a7571f5417b
Fábio Morato <fabiolmorato@dcc.ufmg.br>
2021-05-13 00:14:39 -0300

    chore: testing changes to see how easy it is to tokenize an interpolated template string

--- test.js
+++ test.js
@@ -1,14 +1,43 @@
-import Scanner from "./scanner/Scanner.js";
+import Scanner, { contextTypes } from "./scanner/Scanner.js";
+import Token, { types as tokenTypes } from "./scanner/Token.js";
 
-const scanner = new Scanner(`
+const text = build`${123}
   a<div atributo="lal\\"ala">
     <teste>
-      <maca>
+      <maca exp=${123}>
         <uau atributo="que nao termina o valor" />
-        testando            lalala muito                 bacana isso aí                     
+        <${build} />
+        testando            lalala muito        ${"lalala"}         bacana isso aí                     
       </maca>
     </teste>
   </div>asdasdasd
-`);
+${123}`;
 
-console.log(scanner.lex());
+function build (...args) {
+  const [strings, ...parts] = args;
+  const r = [];
+
+  for (let i = 0; i < strings.length; i++) {
+    r.push(strings[i]);
+    if (i < parts.length) r.push(parts[i]);
+  }
+
+  return r;
+}
+
+const tokens = [];
+let context = contextTypes.CHILDREN;
+
+for (let i = 0; i < text.length; i++) {
+  if (i % 2 === 0) {
+    const scanner = new Scanner(text[i], context);
+    tokens.push(...scanner.lex());
+    context = scanner.context;
+  } else {
+    tokens.push(
+      new Token(tokenTypes.JS_EXP, text[i])
+    );
+  }
+}
+
+console.log(tokens);
